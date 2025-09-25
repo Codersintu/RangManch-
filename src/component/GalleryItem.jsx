27fabaccdb5daff72d4e6f608703d355ed7d85c7
Codersintu <skmahto9199@gmail.com>
2025-09-25 15:39:11 +0530

    added

--- src/component/GalleryItem.jsx
+++ src/component/GalleryItem.jsx
@@ -5,7 +5,7 @@ function GalleryItem({item}) {
   return (
     <div className='flex relative cursor-pointer group' style={{gridRowEnd: `span ${Math.ceil(item.height/150)}`}}>
         <img src={item.media} alt="" className='w-full  object-cover rounded'/>
-        <Link to={`/pin/${item.id}`} className='hidden group-hover:flex flex-col justify-between items-center p-2 absolute top-0 right-0 rounded-lg  h-full '>
+        <Link to={`/pin/${item.id}`} className='hidden group-hover:flex flex-col group-hover:bg-black/30  justify-between items-end p-2 absolute top-0 right-0 rounded-lg w-full  h-full '>
         <button className='p-2 bg-red-600 rounded-lg text-white font-serif'>Save</button>
         <div className="flex gap-2">
           <button className='bg-white rounded-lg'><img className='p-1' src={download} alt="" /></button>
