9ffa6ab0ef9a309da3e086f428da55f5f94d3665
Codersintu <skmahto9199@gmail.com>
2025-09-29 15:00:27 +0530

    added some feature to navigate

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,11 +1,13 @@
 
-import { Link } from "react-router-dom";
+import { Link, Links } from "react-router-dom";
 
 import Imag from "./image/Imag";
+import { useState } from "react";
 function Sidebar() {
+  const [open,setopen]=useState(false)
   return (
     <div className="w-14 md:w-24 z-10 bg-white h-screen border-r border-gray-300 flex flex-col items-center sticky top-0">
-      <div className="flex flex-col items-center justify-between h-full mt-4">
+      <div className="flex relative flex-col items-center justify-between h-full mt-4">
         <div className="flex flex-col gap-10 mt-4 items-center">
           <Link to="/">
           <div className="w-12">
@@ -36,9 +38,18 @@ function Sidebar() {
             <Imag className="w-7" src="../general/bell.png" alt="icon" />
           </div>
         </div>
-        <div className=" rounded-full hover:bg-gray-200">
+        <div onClick={()=>setopen(!open)} className=" rounded-full  hover:bg-gray-200">
           <Imag src="../general/setting.png" className="m-4 cursor-pointer w-8" alt="" />
         </div>
+        {open &&(
+        <div className="flex flex-col absolute bottom-14 -left-5 bg-white shadow-lg border rounded-lg p-4 gap-4">
+          <Link to="/profile">
+            <span className="cursor-pointer font-serif hover:bg-gray-200 ">Profile</span>
+            </Link>
+            <span className="cursor-pointer font-serif hover:bg-gray-200 ">Setting</span>
+            <span className="cursor-pointer font-serif hover:bg-gray-200 ">Logout</span>
+          </div>
+          )}
       </div>
     </div>
   );
